e4ca8ba9cd59ba82996995b825513f4d0a76029a
starshape77 <111961655+starshape7@users.noreply.github.com>
2024-07-04 23:20:22 +0900

    bottomnavigation_floating추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         viewBinding = true
     }
@@ -48,4 +49,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("com.google.android.material:material:1.6.1")
 }
