a0e2a452bf250b0204b8e6ea9ab60d8c11257d12
Chamin Im <ckals413@naver.com>
2024-07-05 01:36:22 +0900

    FEAT: 꿈일기 화면전환

--- app/src/main/java/com/example/umc_hackathon/DreamdiaryContentActivity.kt
+++ app/src/main/java/com/example/umc_hackathon/DreamdiaryContentActivity.kt
@@ -1,5 +1,6 @@
 package com.example.umc_hackathon
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.umc_hackathon.databinding.ActivityDreamdiaryContentBinding
@@ -15,6 +16,12 @@ class DreamdiaryContentActivity: AppCompatActivity() {
 
         binding.dreamdiaryCloseIb.setOnClickListener {
             finish()
+        }
+        binding.dreamdiarySaveTv.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+
         }
     }
 }
--- app/src/main/java/com/example/umc_hackathon/RegisterActivity.kt
+++ app/src/main/java/com/example/umc_hackathon/RegisterActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.umc_hackathon.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
-
     lateinit var binding: ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
