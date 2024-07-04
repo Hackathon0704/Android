6219dd0ffc7374caff017c8d95fdfc4fcab0c92c
starshape77 <111961655+starshape7@users.noreply.github.com>
2024-07-05 03:42:27 +0900

    synk & mypage listener 구현

--- app/src/main/java/com/example/umc_hackathon/MypageDreamShareFriendFragment.kt
+++ app/src/main/java/com/example/umc_hackathon/MypageDreamShareFriendFragment.kt
@@ -1,22 +1,23 @@
-package com.example.umc_hackathon.mypage
+package com.example.umc_hackathon
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.umc_hackathon.databinding.FragmentMypageDreamShareFriendBinding
 import com.example.umc_hackathon.databinding.FragmentMypageManageFriendBinding
 
-class LookVpFragment1 : Fragment() {
+class MypageDreamShareFriendFragment : Fragment() {
 
-    lateinit var binding: FragmentMypageManageFriendBinding
+    lateinit var binding : FragmentMypageDreamShareFriendBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMypageManageFriendBinding.inflate(inflater, container, false)
+        binding = FragmentMypageDreamShareFriendBinding.inflate(inflater, container, false)
 
         return binding.root
     }
