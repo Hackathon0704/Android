d4e4ff302c6ea67181dc3ed87db174868109877f
riadan710 <riadan710@gmail.com>
2024-07-04 21:13:31 +0900

    feat : Login Activity 추가

---
+++ app/src/main/java/com/example/umc_hackathon/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.umc_hackathon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umc_hackathon.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
