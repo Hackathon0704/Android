363850b21622ce95b3584387626b6c6fd95d6123
riadan710 <riadan710@gmail.com>
2024-07-04 21:39:46 +0900

    chore : visibility 수정

--- app/src/main/java/com/example/umc_hackathon/RegisterActivity.kt
+++ app/src/main/java/com/example/umc_hackathon/RegisterActivity.kt
@@ -1,6 +1,7 @@
 package com.example.umc_hackathon
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.umc_hackathon.databinding.ActivityRegisterBinding
 
@@ -10,5 +11,11 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registerPwCheckTv.visibility = View.GONE
+        binding.registerPwReCheckTv.visibility = View.GONE
+        binding.registerNameCheckTv.visibility = View.GONE
     }
 }
