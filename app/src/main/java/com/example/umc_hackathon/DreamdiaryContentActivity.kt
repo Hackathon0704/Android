368887516f3193910e405dc90147a6b28f678f1e
Your Name <you@example.com>
2024-07-04 22:35:56 +0900

    꿈 해몽 댓글달기 페이지

--- app/src/main/java/com/example/umc_hackathon/DreamdiaryContentActivity.kt
+++ app/src/main/java/com/example/umc_hackathon/DreamdiaryContentActivity.kt
@@ -2,8 +2,9 @@ package com.example.umc_hackathon
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.umc_hackathon.databinding.ActivityDreamdiaryContentBinding
 
-class DreamdiaryContentActivity: AppCompatActivity{
+class DreamdiaryContentActivity: AppCompatActivity() {
     lateinit var  binding: ActivityDreamdiaryContentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
