1620db6fbd8fd69625b707e598f3a1b214200bfd
riadan710 <riadan710@gmail.com>
2024-07-04 18:33:06 +0900

    feat : Register Activity 추가

---
+++ app/src/main/java/com/example/umc_hackathon/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.example.umc_hackathon
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.umc_hackathon.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
